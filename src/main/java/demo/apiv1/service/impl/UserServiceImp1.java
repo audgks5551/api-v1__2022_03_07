03be5226634ed8b594f62d9ea34257e9bc3be47c
audgks5551 <audgks5551@daum.net>
2022-03-08 02:03:09 +0900

    회원가입에서 아이디 중복 예외처리 수정

---
+++ src/main/java/demo/apiv1/service/impl/UserServiceImp1.java
@@ -0,0 +1,27 @@
+package demo.apiv1.service.impl;
+
+import demo.apiv1.domain.User;
+import demo.apiv1.exception.UserException;
+import demo.apiv1.repository.UserRepository;
+import demo.apiv1.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserServiceImp1 implements UserService {
+
+    private final UserRepository userRepository;
+
+    public User join(User user) throws UserException {
+        // 회원가입 아이디의 중복이 있는지 확인
+        if (!userRepository.findByUsername(user.getUsername()).isEmpty()) {
+            log.warn("이미 가입된 유저입니다");
+            throw new UserException("이미 가입된 유저입니다", 409);
+         }
+
+        return userRepository.save(user);
+    }
+}
